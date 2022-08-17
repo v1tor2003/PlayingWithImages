d85827b53a49c7cf765c68aa26725f5f52301f93
v1tor2003 <vitor.pr04@hotmail.com>
2022-08-17 11:55:47 -0300

    fixed names

--- scrypt.rb
+++ scrypt.rb
@@ -1,18 +1,19 @@
 require 'mini_magick'
 
 image = MiniMagick::Image.open(ARGV[0])
+
 # Block to get the commandss from the terminal
 ARGV.each do |argument|
   case 
-  # Filters the given img to black and white
+  # Filters the given image to black and white
   when argument == 'to_black_and_white'
     image.colorspace 'gray'
     image.write('output.jpg')
-  # Resizes the given img
+  # Resizes the given image
   when argument.include?("resize")
     image.resize argument.split("=")[1]
     image.write('output.jpg')
-  # Shows the dimensions of the given img 
+  # Shows the dimensions of the given image 
   when argument == 'dimensions'
     print "Dimensions in pixels: #{image.dimensions}"
   end
