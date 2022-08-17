00be4fe058100b21ee132d16b36cf25eb8e2b0b4
v1tor2003 <vitor.pr04@hotmail.com>
2022-08-17 11:53:16 -0300

    initial commit

---
+++ img_trans.rb
@@ -0,0 +1,21 @@
+require 'mini_magick'
+
+image = MiniMagick::Image.open(ARGV[0])
+# Block to get the commandss from the terminal
+ARGV.each do |argument|
+  case 
+  # Filters the given img to black and white
+  when argument == 'to_black_and_white'
+    image.colorspace 'gray'
+    image.write('output.jpg')
+  # Resizes the given img
+  when argument.include?("resize")
+    image.resize argument.split("=")[1]
+    image.write('output.jpg')
+  # Shows the dimensions of the given img 
+  when argument == 'dimensions'
+    print "Dimensions in pixels: #{image.dimensions}"
+  end
+end
+
+
